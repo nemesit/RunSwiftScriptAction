d779d349093c87cce698b894b7ab8ca5579a0996
Felix Grabowski <fukurokujoe@googlemail.com>
2016-11-17 15:49:54 +0100

    Initial Commit

---
+++ Run Swift Script/Run_Swift_Script.m
@@ -0,0 +1,20 @@
+//
+//  Run_Swift_Script.m
+//  Run Swift Script
+//
+//  Created by Felix Grabowski on 17/11/2016.
+//  Copyright © 2016 FelixGrabowski. All rights reserved.
+//
+
+#import "Run_Swift_Script.h"
+
+@implementation Run_Swift_Script
+
+- (id)runWithInput:(id)input fromAction:(AMAction *)anAction error:(NSDictionary **)errorInfo
+{
+    // Add your code here, returning the data to be passed to the next action.
+    
+    return input;
+}
+
+@end
